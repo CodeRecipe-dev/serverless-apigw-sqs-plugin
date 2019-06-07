39d83663290745e2db207a13756d1abda0bc4c7e
Rohit Verma <rohitverma@live.ca>
2019-06-06 21:57:30 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,121 @@
+'use strict';
+var AWS = require('aws-sdk');
+var fs = require('fs');
+
+class ServerlessApiGWSqsPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.apiEndpointUrl = "";
+    this.commands = {
+      deploy: {
+        lifecycleEvents: [
+        'resources'
+        ]
+      },
+    };
+
+    this.hooks = {
+      'before:deploy:deploy': this.beforeDeployResources.bind(this),
+      'before:remove:remove': this.deleteStack.bind(this),
+      'after:aws:info:displayEndpoints': this.setApiEndpoint.bind(this)
+    };
+  }
+  setCloudFormation() {
+    AWS.config.update({region:this.serverless.service.provider.region});
+    this.cloudformation = new AWS.CloudFormation();
+  }
+  setApiEndpoint() {
+    this.setCloudFormation()
+    var stackName = this.getStackName(this.options.stage, this.serverless.service.service)
+    return new Promise((resolve, reject) => {
+      this.cloudformation.describeStacks({StackName: stackName}, function(err, data) {
+        if(!err) {
+          console.log("  POST - "+data["Stacks"][0]["Outputs"][0]["OutputValue"]);
+          resolve();
+        }
+      });
+    });
+  }
+  deleteStack() {
+    this.setCloudFormation()
+    var stackName = this.getStackName(this.options.stage, this.serverless.service.service)
+    
+    this.cloudformation.deleteStack({StackName: stackName}, function (err, data) {
+      if (err) {
+        this.serverless.cli.log("[CodeRecipe ApiGW SQS Plugin] Error: ", err.message);
+      }
+      else {
+        this.serverless.cli.log("[CodeRecipe ApiGW SQS Plugin] Deleted ApiGW -> SQS");
+      }
+    }.bind(this));
+  }
+  getStackName(stage, serviceName) {
+    return stage + "-" + serviceName + "-APIGW-SQS";
+  }
+  beforeDeployResources() {
+    this.setCloudFormation()
+    return new Promise((resolve, reject) => {
+      fs.readFile('template.yml', 'utf8', (err, contents) => {
+        var stackName = this.getStackName(this.options.stage, this.serverless.service.service)
+        var apiEndpoint = this.serverless.service.custom.apiGwSqs.apiEndpoint
+        var queueName = this.serverless.service.custom.apiGwSqs.queueName
+        var replaceStageName = new RegExp('STAGE_NAME', 'g');
+        var replaceApiEndpoint = new RegExp('API_ENDPOINT', 'g');
+        var replaceQueueName = new RegExp('QUEUE_NAME', 'g');
+        contents = contents.replace(replaceStageName, this.options.stage);
+        contents = contents.replace(replaceApiEndpoint, apiEndpoint);
+        contents = contents.replace(replaceQueueName, queueName);
+        var params = {
+          Capabilities: [
+            'CAPABILITY_IAM'
+          ],
+          StackName: stackName,
+          TemplateBody: contents,
+        };
+        this.cloudformation.createStack(params, (err, data) => {
+          if (err) {
+            if(err.code == 'AlreadyExistsException') {
+                // update stack instead
+                this.cloudformation.updateStack(params, (err, data) => {
+                  if (err) {
+                    if (err.message == "No updates are to be performed.") {
+                      this.serverless.cli.log("[CodeRecipe ApiGW SQS Plugin] No changes to ApiGW -> SQS")
+                      resolve();
+                    }
+                  } else {
+                    this.cloudformation.waitFor('stackCreateComplete', {StackName: stackName}, (err, data) => {
+                      if (err) {
+                        console.log(err.message);
+                      } 
+                      else {
+                        this.serverless.cli.log("[CodeRecipe ApiGW SQS Plugin] Updated ApiGW -> SQS")
+                      }
+                      resolve();
+                    });
+                  }
+                });
+              } else {
+                this.serverless.cli.log("[CodeRecipe ApiGW SQS Plugin] Error: ", err.message);
+                resolve();
+              }
+            }
+          else {
+            this.serverless.cli.log("[CodeRecipe ApiGW SQS Plugin] Creating Api GW -> SQS")
+            this.cloudformation.waitFor('stackCreateComplete', {StackName: stackName}, (err, data) => {
+              if (err) {
+                console.log(err.message);
+              } 
+              else {
+                this.serverless.cli.log("[CodeRecipe ApiGW SQS Plugin] Created Api GW -> SQS")
+              }
+              resolve();
+            });
+          }
+        });
+      });
+    })
+  }
+}
+
+module.exports = ServerlessApiGWSqsPlugin;
